ee3270415f873e870db8e8894dc937a5106b56eb
khggc <khggcvfd@gmail.com>
2020-08-17 22:53:27 +0500

    Картинка купона с текстом теперь ссылки.   Подключил плагин для масок телефона.

--- src/js/main.js
+++ src/js/main.js
@@ -78,3 +78,13 @@ $(document).ready(function () {
         $('body').css('overflow', 'auto')
     })
 });
+
+
+
+
+var elements = document.getElementsByClassName("delivery__mask");
+for (var i = 0; i < elements.length; i++) {
+    new IMask(elements[i], {
+        mask: '+{7} (000) 000-00-00',
+    });
+}
